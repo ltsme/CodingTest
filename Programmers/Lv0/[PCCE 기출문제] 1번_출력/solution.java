250364de0ea5e0bcddf9b8998cc2b2fec3ea3fef
YangHyeonJoon <guswns2415@naver.com>
2024-02-06 04:37:21 +0900

    Initial commit

---
+++ Programmers/Lv0/[PCCE 기출문제] 1번_출력/solution.java
@@ -0,0 +1,31 @@
+// import java.util.Scanner;
+
+// public class Solution {
+//     public static void main(String[] args) {
+//         String msg = [?];
+//         int val1 = [?];
+//         String val2 = [?];
+
+//         System.out.println(msg);
+//         System.out.println(val1 + 10);
+//         System.out.println(val2 + "10");
+//     }
+// }
+
+// Spring is beginning
+// 13
+// 310
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+  String msg = "Spring is beginning";
+        int val1 = 3;
+        String val2 = "3";
+
+        System.out.println(msg);
+        System.out.println(val1 + 10);
+        System.out.println(val2 + "10");
+    }
+}
